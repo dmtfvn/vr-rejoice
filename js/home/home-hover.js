57e60cdf8c72805f4b8d602dddcbf23f838be525
dmtfvn <dmtfvn@gmail.com>
2024-05-28 20:24:45 +0300

    Update site for mobile

--- js/home/home-hover.js
+++ js/home/home-hover.js
@@ -1,9 +1,5 @@
+import { hover } from "../../hover-func.js";
+
 const circle = document.querySelectorAll('.js-circle');
 
-['mouseenter', 'mouseleave'].forEach( evt => {
-  circle.forEach( item => {
-    item.addEventListener(evt, function () {
-      item.classList.toggle('active');
-    });
-  });
-});
+hover(circle);
