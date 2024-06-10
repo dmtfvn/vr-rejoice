c2ab2397dabb55aa8271bf40b7f23b1ddcc9710c
dmtfvn <dmtfvn@gmail.com>
2024-06-10 22:06:53 +0300

    Rearrange functions in faq-toggle.js

--- js/faq/faq-toggle.js
+++ js/faq/faq-toggle.js
@@ -1,8 +1,17 @@
 const arrow = document.querySelectorAll('.js-arrow');
 const faq = document.querySelectorAll('.js-faq');
 
+function set(elem) {
+  elem.classList.toggle('active');
+}
+
 arrow.forEach( item => {
   item.addEventListener('click', function () {
+    function showHideAnswer(arrowIndex, faqIndex) {
+      if (item === arrowIndex) {
+        set(faqIndex);
+      }
+    }
 
     showHideAnswer(arrow[0], faq[0]);
     showHideAnswer(arrow[1], faq[1]);
@@ -12,15 +21,5 @@ arrow.forEach( item => {
     showHideAnswer(arrow[5], faq[5]);
     showHideAnswer(arrow[6], faq[6]);
     showHideAnswer(arrow[7], faq[7]);
-
-    function showHideAnswer(arrowIndex, faqIndex) {
-      if (item === arrowIndex) {
-        set(faqIndex);
-      }
-    }
   });
-
-  function set(elem) {
-    elem.classList.toggle('active');
-  }
 });
