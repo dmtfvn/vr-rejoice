2118b1764dd97f6a3e636307e6a43c5cb1c47a07
dmtfvn <dmtfvn@gmail.com>
2024-07-28 14:57:06 +0300

    Correct variables naming in faq-form.js

--- js/faq/faq-form.js
+++ js/faq/faq-form.js
@@ -5,21 +5,21 @@ const closeForm = document.querySelector('.js-cancel-form');
 
 const form = document.getElementById('form');
 
-const user = document.getElementById('name');
-const query = document.getElementById('text');
-const email = document.getElementById('email');
+const inputUser = document.getElementById('name');
+const inputQuery = document.getElementById('query');
+const inputEmail = document.getElementById('email');
 
-const errorUser = document.querySelector('.js-error-msg-name');
-const errorQuery = document.querySelector('.js-error-msg-question');
-const errorEmail = document.querySelector('.js-error-msg-email');
+const errorMsgUser = document.querySelector('.js-error-msg-name');
+const errorMsgQuery = document.querySelector('.js-error-msg-query');
+const errorMsgEmail = document.querySelector('.js-error-msg-email');
 
 function eraseDataAndMsg() {
-  user.value = null;
-  errorUser.innerText = null;
-  query.value = null;
-  errorQuery.innerText = null;
-  email.value = null;
-  errorEmail.innerText = null;
+  inputUser.value = null;
+  errorMsgUser.innerText = null;
+  inputQuery.value = null;
+  errorMsgQuery.innerText = null;
+  inputEmail.value = null;
+  errorMsgEmail.innerText = null;
 }
 
 openForm.addEventListener('click', function () {
@@ -50,28 +50,28 @@ function removeErrorMsg(step, error) {
   }
 }
 
-[user, query, email].forEach( elem => {
+[inputUser, inputQuery, inputEmail].forEach( elem => {
   elem.addEventListener('input', function () {
-    curUser = user.value.trim();
-    curQuery = query.value.trim();
-    curEmail = email.value.trim();
+    curUser = inputUser.value.trim();
+    curQuery = inputQuery.value.trim();
+    curEmail = inputEmail.value.trim();
 
     if (curUser.length !== 0 && curUser.length < 2) {
-      errorUser.innerText = 'Name must be longer than 1 character';
+      errorMsgUser.innerText = 'Name must be longer than 1 character';
     } else {
-      removeErrorMsg(user, errorUser);
+      removeErrorMsg(inputUser, errorMsgUser);
     }
 
     if (curQuery.length !== 0 && curQuery.length <= 30) {
-      errorQuery.innerText = 'Question must be longer than 30 characters';
+      errorMsgQuery.innerText = 'Question must be longer than 30 characters';
     } else {
-      removeErrorMsg(query, errorQuery);
+      removeErrorMsg(inputQuery, errorMsgQuery);
     }
 
     if (curEmail.length !== 0 && curEmail.length <= 7) {
-      errorEmail.innerText = 'Email must be longer than 7 characters';
+      errorMsgEmail.innerText = 'Email must be longer than 7 characters';
     } else {
-      removeErrorMsg(email, errorEmail);
+      removeErrorMsg(inputEmail, errorMsgEmail);
     }
   });
 });
@@ -84,22 +84,22 @@ form.addEventListener('submit', function (e) {
   if (curUser === '' || curUser.length !== 0 && curUser.length < 2) {
     e.preventDefault();
 
-    errorUser.innerText = 'Please enter your name';
-    user.focus();
+    errorMsgUser.innerText = 'Please enter your name';
+    inputUser.focus();
   } else if (curQuery === '' || curQuery.length !== 0 && curQuery.length <= 30) {
     e.preventDefault();
 
-    errorQuery.innerText = 'Please enter your question';
-    query.focus();
+    errorMsgQuery.innerText = 'Please enter your question';
+    inputQuery.focus();
   } else if (curEmail === '' || curEmail.length !== 0 && curEmail.length <= 7) {
     e.preventDefault();
 
-    errorEmail.innerText = 'Please enter your email';
-    email.focus();
+    errorMsgEmail.innerText = 'Please enter your email';
+    inputEmail.focus();
   } else if (!isEmailValid(curEmail)) {
     e.preventDefault();
 
-    errorEmail.innerText = 'This email is not valid';
-    email.focus();
+    errorMsgEmail.innerText = 'This email is not valid';
+    inputEmail.focus();
   }
 });
