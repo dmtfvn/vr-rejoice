2b96c0cc3fcbf111d4404b46d7cc0e7a7176ef75
dmtfvn <dmtfvn@gmail.com>
2024-12-17 11:11:32 +0200

    Separate header, sidebar, and footer into folders

---
+++ _footer/footer.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', createFooter());
+
+function createFooter() {
+  const footerEl = document.getElementById('footer');
+
+  const copyright = String.fromCodePoint(169);
+  const dot = String.fromCharCode(183);
+
+  const paraEl = document.createElement('p');
+  paraEl.className = 'copyright';
+
+  const spanEl = document.createElement('span');
+  spanEl.className = 'rejoice';
+  spanEl.textContent = `${dot} rejoice`;
+
+  paraEl.appendChild(spanEl);
+
+  paraEl.insertAdjacentHTML(
+    'afterbegin',
+    `Copyright ${copyright} 2024 `
+  );
+
+  paraEl.insertAdjacentHTML(
+    'beforeend',
+    ' Inc. All rights reserved.'
+  );
+
+  footerEl.appendChild(paraEl);
+}
---
+++ _header/header.js
@@ -0,0 +1,46 @@
+import { createLinkEl } from '../helpers/createLink.js';
+import { createSvgEl } from '../helpers/createSvg.js';
+
+window.addEventListener('DOMContentLoaded', createNavbar());
+
+function createNavbar() {
+  const headerEl = document.getElementById('header');
+
+  const linkWrapper = document.createElement('div');
+  linkWrapper.className = 'link-container';
+
+  const logoEl = createLinkEl('logo-container', 'index.html');
+
+  const imgEl = document.createElement('img');
+  imgEl.className = 'logo';
+  imgEl.src = 'logo/logo.png';
+  imgEl.alt = 'logotype image';
+
+  logoEl.appendChild(imgEl);
+
+  const openSbBtn = document.createElement('button');
+  openSbBtn.classList.add('hb-icon');
+  openSbBtn.classList.add('js-hb-icon');
+
+  openSbBtn.appendChild(
+    createSvgEl('M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z')
+  );
+
+  linkWrapper.appendChild(logoEl);
+  linkWrapper.appendChild(createLinkEl('header-link', 'explore.html', 'Explore'));
+  linkWrapper.appendChild(createLinkEl('header-link', 'vision.html', 'Vision'));
+  linkWrapper.appendChild(createLinkEl('header-link', 'faq.html', 'FAQ'));
+  linkWrapper.appendChild(createLinkEl('header-link', 'manifest.html', 'Manifest'));
+
+  headerEl.appendChild(linkWrapper);
+  headerEl.appendChild(openSbBtn);
+}
+
+document.querySelector('.js-hb-icon').addEventListener('click', () => showSidebar());
+
+function showSidebar() {
+  const element = document.querySelector('.js-sidebar-container');
+
+  element.style.display = 'flex';
+  document.body.classList.add('stop-y-scroll');
+}
