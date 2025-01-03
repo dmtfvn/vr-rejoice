ffc0e65b2e1e0997cb290bd1c619555430a167d2
dmtfvn <dmtfvn@gmail.com>
2025-01-03 20:11:15 +0200

    Use css to implement hover instead of home-hover.js

--- scripts/home/home-hover.js
+++
@@ -1,9 +0,0 @@
-const circle = document.querySelectorAll('.js-circle');
-
-['mouseenter', 'mouseleave'].forEach( evt => {
-  circle.forEach( item => {
-    item.addEventListener(evt, function () {
-      item.classList.toggle('active');
-    });
-  });
-});
