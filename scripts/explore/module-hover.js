0a27543fc40f88b03a0e67a77c44b08287399bb6
dmtfvn <dmtfvn@gmail.com>
2025-01-04 18:32:15 +0200

    Correct explore.html semantically and refactor its css

--- scripts/explore/module-hover.js
+++ scripts/explore/module-hover.js
@@ -21,23 +21,23 @@ function insertName(subj, value) {
 
 module.forEach(elem => {
   elem.addEventListener('mouseenter', function () {
-    function hoverOn(obj, txt, name) {
+    function hoverModule(obj, txt, name) {
       if (elem === obj) {
         insertName(txt, name);
       }
     }
 
-    hoverOn(module[0], text[0], title[0]);
-    hoverOn(module[1], text[1], title[1]);
-    hoverOn(module[2], text[2], title[2]);
-    hoverOn(module[3], text[3], title[3]);
-    hoverOn(module[4], text[4], title[4]);
-    hoverOn(module[5], text[5], title[5]);
-    hoverOn(module[6], text[6], title[6]);
-    hoverOn(module[7], text[7], title[7]);
-    hoverOn(module[8], text[8], title[8]);
-    hoverOn(module[9], text[9], title[9]);
-    hoverOn(module[10], text[10], title[10]);
+    hoverModule(module[0], text[0], title[0]);
+    hoverModule(module[1], text[1], title[1]);
+    hoverModule(module[2], text[2], title[2]);
+    hoverModule(module[3], text[3], title[3]);
+    hoverModule(module[4], text[4], title[4]);
+    hoverModule(module[5], text[5], title[5]);
+    hoverModule(module[6], text[6], title[6]);
+    hoverModule(module[7], text[7], title[7]);
+    hoverModule(module[8], text[8], title[8]);
+    hoverModule(module[9], text[9], title[9]);
+    hoverModule(module[10], text[10], title[10]);
   });
 });
 
